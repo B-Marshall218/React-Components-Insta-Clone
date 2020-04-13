6c93ffedbce6132fdf7acd2dc385b076f521370a
Brian <lawmen218@yahoo.com>
2020-04-12 21:23:40 -0400

    everything done except for instagram logo and like button usage

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,11 +6,14 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const [commentData] = useState(props.comments)
+  console.log("from comment section: ", commentData)
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {commentData.map((comment, index) => {
+        return <Comment key={index} comment={comment} />
+      })}
+      < CommentInput />
     </div>
   );
 };
